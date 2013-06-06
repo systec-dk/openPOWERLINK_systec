815ddcadcc2d6c193205d6138bae1e8c4cf7319c
Thomas Mair <thomas.mair@br-automation.com>
2013-06-06 16:03:45 +0200

    [TASK] Change filters to work with Veth driver
    * Move filter ID's to seperate header for access in the EdrvOpenMac
      -> This ID's enable the use of a different hook for Veth frames.
    * The Veth driver uses late release to copy the payload of the frame
      in the background.
    
    Change-Id: I9f91f6793f9d85dcacfa29d29d08a343894be40a
    Reviewed-on: http://brateggevoat1/gerrit/973
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>

--- EplStack/EplDllk.c
+++ EplStack/EplDllk.c
@@ -72,6 +72,7 @@
 #include "kernel/EplDllkCal.h"
 #include "kernel/EplEventk.h"
 #include "kernel/EplErrorHandlerk.h"
+#include "kernel/EplDllkFilter.h"
 #include "EplNmt.h"
 #include "edrv.h"
 #include "Benchmark.h"
@@ -194,29 +195,6 @@
                                     // on MN: 7 + MaxPReq of regular CNs + 1 Diag + 1 Router
 #else
   #define EPL_DLLK_TXFRAME_COUNT    (EPL_DLLK_TXFRAME_PRES + 2)
-#endif
-
-
-#define EPL_DLLK_FILTER_PREQ          0
-#define EPL_DLLK_FILTER_SOA_IDREQ     1
-#define EPL_DLLK_FILTER_SOA_STATREQ   2
-#define EPL_DLLK_FILTER_SOA_NMTREQ    3
-#if EPL_DLL_PRES_CHAINING_CN != FALSE
-  #define EPL_DLLK_FILTER_SOA_SYNCREQ 4
-  #define EPL_DLLK_FILTER_SOA_NONEPL  5
-#else
-  #define EPL_DLLK_FILTER_SOA_NONEPL  4
-#endif
-
-#define EPL_DLLK_FILTER_SOA           (EPL_DLLK_FILTER_SOA_NONEPL + 1)
-#define EPL_DLLK_FILTER_SOC           (EPL_DLLK_FILTER_SOA + 1)
-#define EPL_DLLK_FILTER_ASND          (EPL_DLLK_FILTER_SOC + 1)
-#define EPL_DLLK_FILTER_PRES          (EPL_DLLK_FILTER_ASND + 1)
-
-#if EPL_DLL_PRES_FILTER_COUNT < 0
-  #define EPL_DLLK_FILTER_COUNT       (EPL_DLLK_FILTER_PRES + 1)
-#else
-  #define EPL_DLLK_FILTER_COUNT       (EPL_DLLK_FILTER_PRES + EPL_DLL_PRES_FILTER_COUNT)
 #endif
 
 #define EPL_DLLK_SOAREQ_COUNT       3
@@ -1935,7 +1913,7 @@ tEplDllkNodeInfo*   pIntNodeInfo;
         }
 
 #else
-        for (uiHandle = EPL_DLLK_FILTER_PRES; uiHandle < EPL_DLLK_FILTER_COUNT; uiHandle++)
+        for (uiHandle = EPL_DLLK_FILTER_PRES; uiHandle < (EPL_DLLK_FILTER_PRES + EPL_DLL_PRES_FILTER_COUNT); uiHandle++)
         {
             AmiSetQword48ToBe(&EplDllkInstance_g.m_aFilter[uiHandle].m_abFilterValue[0],
                               EPL_C_DLL_MULTICAST_PRES);
@@ -1967,13 +1945,36 @@ tEplDllkNodeInfo*   pIntNodeInfo;
                 EplDllkInstance_g.m_aFilter[uiHandle].m_fEnable = TRUE;
 
                 uiHandle++;
-                if (uiHandle >= EPL_DLLK_FILTER_COUNT)
+                if (uiHandle >= (EPL_DLLK_FILTER_PRES + EPL_DLL_PRES_FILTER_COUNT))
                 {
                     break;
                 }
             }
         }
 #endif
+#endif
+
+#if(((EPL_MODULE_INTEGRATION) & (EPL_MODULE_VETH)) != 0)
+    //Add veth filter for Unicast address
+    EPL_MEMSET(EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_UNICAST].m_abFilterMask, 0xFF, 6);
+
+    EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_UNICAST].m_pTxBuffer = NULL;  // No auto response
+
+    EPL_MEMCPY(EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_UNICAST].m_abFilterValue,
+            &EplDllkInstance_g.m_be_abLocalMac[0], 6);
+
+    EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_UNICAST].m_fEnable = TRUE;
+
+    //Add veth filter for Broadcast address
+    EPL_MEMSET(EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_BROADCAST].m_abFilterMask, 0xFF, 6);
+
+    EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_BROADCAST].m_pTxBuffer = NULL;  // No auto response
+
+    EPL_MEMCPY(EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_BROADCAST].m_abFilterValue,
+            EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_BROADCAST].m_abFilterMask,
+            sizeof(EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_BROADCAST].m_abFilterValue));
+
+    EplDllkInstance_g.m_aFilter[EPL_DLLK_FILTER_VETH_BROADCAST].m_fEnable = TRUE;
 #endif
 
 #if (EPL_DLL_PROCESS_SYNC == EPL_DLL_PROCESS_SYNC_ON_TIMER)
---
+++ Include/kernel/EplDllkFilter.h
@@ -0,0 +1,118 @@
+/****************************************************************************
+
+  (c) Bernecker + Rainer Industrie-Elektronik Ges.m.b.H.
+      A-5142 Eggelsberg, B&R Strasse 1
+      www.br-automation.com
+
+
+  Project:      openPOWERLINK
+
+  Description:  include file for the EplDllk filters
+
+  License:
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. Neither the name of SYSTEC electronic GmbH nor the names of its
+       contributors may be used to endorse or promote products derived
+       from this software without prior written permission. For written
+       permission, please contact info@systec-electronic.com.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+
+    Severability Clause:
+
+        If a provision of this License is or becomes illegal, invalid or
+        unenforceable in any jurisdiction, that shall not affect:
+        1. the validity or enforceability in that jurisdiction of any other
+           provision of this License; or
+        2. the validity or enforceability in other jurisdictions of that or
+           any other provision of this License.
+
+  -------------------------------------------------------------------------
+
+                $Author$
+
+                $Revision$  $Date$
+
+                $State$
+
+                Build Environment:
+                    GCC V3.4
+
+****************************************************************************/
+
+#ifndef _EPLDLLKFILTER_H_
+#define _EPLDLLKFILTER_H_
+
+#include "EplCfg.h"
+
+//---------------------------------------------------------------------------
+// const defines
+//---------------------------------------------------------------------------
+
+#define EPL_DLLK_FILTER_PREQ          0
+#define EPL_DLLK_FILTER_SOA_IDREQ     1
+#define EPL_DLLK_FILTER_SOA_STATREQ   2
+#define EPL_DLLK_FILTER_SOA_NMTREQ    3
+#if EPL_DLL_PRES_CHAINING_CN != FALSE
+  #define EPL_DLLK_FILTER_SOA_SYNCREQ 4
+  #define EPL_DLLK_FILTER_SOA_NONEPL  5
+#else
+  #define EPL_DLLK_FILTER_SOA_NONEPL  4
+#endif
+
+#define EPL_DLLK_FILTER_SOA           (EPL_DLLK_FILTER_SOA_NONEPL + 1)
+#define EPL_DLLK_FILTER_SOC           (EPL_DLLK_FILTER_SOA + 1)
+#define EPL_DLLK_FILTER_ASND          (EPL_DLLK_FILTER_SOC + 1)
+#define EPL_DLLK_FILTER_PRES          (EPL_DLLK_FILTER_ASND + 1)
+
+#if(((EPL_MODULE_INTEGRATION) & (EPL_MODULE_VETH)) != 0)
+  #if EPL_DLL_PRES_FILTER_COUNT < 0
+    #define EPL_DLLK_FILTER_VETH_UNICAST      (EPL_DLLK_FILTER_PRES + 1)
+  #else
+    #define EPL_DLLK_FILTER_VETH_UNICAST      (EPL_DLLK_FILTER_PRES + EPL_DLL_PRES_FILTER_COUNT)
+  #endif
+  #define EPL_DLLK_FILTER_VETH_BROADCAST    (EPL_DLLK_FILTER_VETH_UNICAST + 1)
+
+  #define EPL_DLLK_FILTER_COUNT                (EPL_DLLK_FILTER_VETH_BROADCAST + 1)
+#else
+  #if EPL_DLL_PRES_FILTER_COUNT < 0
+    #define EPL_DLLK_FILTER_COUNT       (EPL_DLLK_FILTER_PRES + 1)
+  #else
+    #define EPL_DLLK_FILTER_COUNT       (EPL_DLLK_FILTER_PRES + EPL_DLL_PRES_FILTER_COUNT)
+  #endif
+#endif
+
+//---------------------------------------------------------------------------
+// typedef
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+// function prototypes
+//---------------------------------------------------------------------------
+
+
+#endif /* _EPLDLLKFILTER_H_ */
+
