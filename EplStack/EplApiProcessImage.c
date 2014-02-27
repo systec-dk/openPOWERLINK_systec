6a42937b310518384228a9ba02ec973012e8cb00
Romain Naour <romain.naour@openwide.fr>
2014-02-27 11:47:04 +0100

    [FIX] EplApiProcessImage: reset PIs data after allocation.
    The PIs data can be uninitialized until the application uses it.
    It may be dangerous to set by default an output.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

--- EplStack/EplApiProcessImage.c
+++ EplStack/EplApiProcessImage.c
@@ -334,6 +334,7 @@ unsigned int    fShbNewCreated;
         Ret = kEplApiPIOutOfMemory;
         goto Exit;
     }
+    EPL_MEMSET(EplApiProcessImageInstance_g.m_In.m_pImage, 0, sizeof(uiSizeProcessImageIn_p));
     EplApiProcessImageInstance_g.m_In.m_uiSize = uiSizeProcessImageIn_p;
 
     EplApiProcessImageInstance_g.m_Out.m_pImage = EPL_MALLOC(uiSizeProcessImageOut_p);
@@ -342,6 +343,7 @@ unsigned int    fShbNewCreated;
         Ret = kEplApiPIOutOfMemory;
         goto Exit;
     }
+    EPL_MEMSET(EplApiProcessImageInstance_g.m_Out.m_pImage, 0, sizeof(uiSizeProcessImageOut_p));
     EplApiProcessImageInstance_g.m_Out.m_uiSize = uiSizeProcessImageOut_p;
 
     TRACE("%s: Alloc(%p, %u, %p, %u)\n",
